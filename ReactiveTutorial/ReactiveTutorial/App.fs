2c60e43d96cccfc0c75434e413c498c9ea5a5008
Stephen Elliott <steell1890@gmail.com>
2014-03-27 23:27:27 -0400

    project created
    Generated a new Project using the Online Template:  F# Empty Windows App
    (WPF)

---
+++ ReactiveTutorial/ReactiveTutorial/App.fs
@@ -0,0 +1,19 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+
+open FSharpx
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+let loadWindow() =
+   let window = MainWindow()
+   
+   // Your awesome code goes here and you have strongly typed access to the XAML via "window"
+   
+   window.Root
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
